27193669ce5d9c175d8dadd6c54794cee464ccaf
TSGLOBALTECH <team@tsglobaltech.com>
2026-01-20 18:11:20 +0530

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -117,8 +117,8 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico', sizes: 'any' },
-      { url: '/images/logo-192x192.png', type: 'image/png', sizes: '192x192' },
-      { url: '/images/logo-512x512.png', type: 'image/png', sizes: '512x512' },
+      { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+      { url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
     ],
     apple: '/images/apple-touch-icon.png',
     shortcut: '/favicon.ico',
